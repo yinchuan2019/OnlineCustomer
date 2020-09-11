7a3fddd774ae3e2a205d42a7ae91a1420f2b3d85
abel <abel09@qq.com>
2020-09-11 14:47:26 +0800

    init

---
+++ im-gateway/src/main/java/com/im/gateway/GatewayApplication.java
@@ -0,0 +1,18 @@
+package com.im.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * Created by abel on 2020/9/11
+ * GatewayApplication
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
