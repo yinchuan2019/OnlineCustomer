85c9648cfd0634e816ceb44e82f3c411249c349c
abel <abel09@qq.com>
2020-09-17 11:45:10 +0800

    init

--- im-websocket-server/src/main/java/com/im/websocket/config/WebSocketConfig.java
+++ im-websocket-server/src/main/java/com/im/websocket/config/WebSocketConfig.java
@@ -1,30 +1,60 @@
 package com.im.websocket.config;
 
+import com.im.websocket.Interceptor.HeaderParamInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
+
 
 @Configuration
 @EnableWebSocket
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+    @Autowired
+    private HeaderParamInterceptor headerParamInterceptor;
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        // 注册一个 /notification 端点，前端通过这个端点进行连接
-        registry.addEndpoint("/notification")
-                //解决跨域问题
-                .setAllowedOrigins("*")
-                .withSockJS();
+        registry.addEndpoint("/endpointChat").setAllowedOrigins("*").withSockJS();
     }
+
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         //定义了一个客户端订阅地址的前缀信息，也就是客户端接收服务端发送消息的前缀信息
         registry.enableSimpleBroker("/topic");
+
+//        registry.enableStompBrokerRelay("/topic")
+//                .setRelayHost("localhost")
+//                .setRelayPort(61613)
+//                .setClientLogin("guest")
+//                .setClientPasscode("guest");
+
+        //定义了服务端接收地址的前缀，也即客户端给服务端发消息的地址前缀
+        registry.setApplicationDestinationPrefixes("/app");
+        // 点对点使用的订阅前缀（客户端订阅路径上会体现出来），不设置的话，默认也是/user/
+        registry.setUserDestinationPrefix("/user/");
+    }
+
+    /**
+     * 采用自定义拦截器，获取connect时候传递的参数
+     *
+     * @param registration
+     */
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(headerParamInterceptor);
     }
 
 }
